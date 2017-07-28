9e43dff5b253009546bb279c89158087a442425c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-28 13:39:09 -0400

    Update Abstractions.jl

--- src/Abstractions.jl
+++ src/Abstractions.jl
@@ -31,7 +31,38 @@ const MachUInt  = Union{UInt64, UInt32, UInt16, UInt8}
 const FastFloat = Float32
 const FastInt   = Int32
 const FastUInt  = UInt32
-       
+
+# from StatsBase.jl/common.jl
+# common utilities
+
+## convenient type alias
+#
+#  These types signficantly reduces the need of using
+#  type parameters in functions (which are often just
+#  for the purpose of restricting the arrays to real)
+#
+# These could be removed when the Base supports
+# covariant type notation, i.e. AbstractVector{<:Real}
+#
+
+const RealArray{T<:Real,N} = AbstractArray{T,N}
+const RealVector{T<:Real} = AbstractArray{T,1}
+const RealMatrix{T<:Real} = AbstractArray{T,2}
+
+const IntegerArray{T<:Integer,N} = AbstractArray{T,N}
+const IntegerVector{T<:Integer} = AbstractArray{T,1}
+const IntegerMatrix{T<:Integer} = AbstractArray{T,2}
+
+const RealFP = Union{Float32, Float64}
+
+## conversion from real to fp types
+
+fptype{T<:Union{Float32,Bool,Int8,UInt8,Int16,UInt16}}(::Type{T}) = Float32
+fptype{T<:Union{Float64,Int32,UInt32,Int64,UInt64,Int128,UInt128}}(::Type{T}) = Float64
+fptype(::Type{Complex64}) = Complex64
+fptype(::Type{Complex128}) = Complex128
+
+# end from StatsBase
 
 import Base.Dates: AbstractTime, TimeType
 import TimeSeries: AbstractTimeSeries
